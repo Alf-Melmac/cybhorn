87618a780256bb89fa0e6a744fa6ccb29dab125e
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-03-17 18:33:39 +0100

    Added content for start page Unified page layout Beautified frontend #rainbow

--- src/main/java/de/inhorn/cybhorn/controller/web/StartWebController.java
+++ src/main/java/de/inhorn/cybhorn/controller/web/StartWebController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 /**
  * @author Alf
  * @since 17.03.2021
@@ -12,6 +15,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class StartWebController {
 	@GetMapping("/")
 	public ModelAndView getStart() {
-		return new ModelAndView("start");
+		ModelAndView mav = new ModelAndView("start");
+
+		mav.addObject("subscribersUrl", linkTo(methodOn(SubscriberWebController.class).getSubscribers()));
+		mav.addObject("subscriptionsUrl", linkTo(methodOn(SubscriptionWebController.class).getSubscriptions("")));
+		mav.addObject("terminalsUrl", linkTo(methodOn(TerminalWebController.class).getTerminals("")));
+
+		return mav;
 	}
 }
