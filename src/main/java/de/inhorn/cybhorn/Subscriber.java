8f8fcba90b78b75fefccb10956c1dfbe8d99bfb8
Karima Ben Fradj <karimabf@outlook.com>
2021-03-17 10:17:17 +0100

    create Subscriber and Subscriptiontype classes

---
+++ src/main/java/de/inhorn/cybhorn/Subscriber.java
@@ -0,0 +1,24 @@
+package de.inhorn.cybhorn;
+
+
+public class Subscriber {
+
+
+
+    private String[] IMSI = new String[3];
+    private String terminalType;
+    private Subscriptiontype subscriptionType;
+    private int secondsCalled;
+    private double dataUsed;  //in MB
+
+
+    public Subscriber(){
+
+    }
+
+    public void clearAll(){
+
+    }
+
+
+}
---
+++ src/main/java/de/inhorn/cybhorn/Subscriptiontype.java
@@ -0,0 +1,14 @@
+package de.inhorn.cybhorn;
+
+
+public class Subscriptiontype{
+
+    private int basicFee;
+    private int minutesIncluded;
+    private int pricePerMinute;
+    private double dataVolume;
+
+    public Subscriptiontype(){
+
+    }
+}
