8b28de8c499bf6b0ac618c0ffcb1fc9729a481f2
Karima Ben Fradj <karimabf@outlook.com>
2021-03-17 09:59:25 +0100

    Initial commit

---
+++ src/main/java/de/inhorn/cybhorn/CybhornApplication.java
@@ -0,0 +1,13 @@
+package de.inhorn.cybhorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybhornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybhornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/inhorn/cybhorn/CybhornApplicationTests.java
@@ -0,0 +1,13 @@
+package de.inhorn.cybhorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybhornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
