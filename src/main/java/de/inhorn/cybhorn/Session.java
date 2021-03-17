f971bff8e5337788b9b85580412a946836e4a36e
Karima Ben Fradj <karimabf@outlook.com>
2021-03-17 14:52:25 +0100

    Update Session.java

--- src/main/java/de/inhorn/cybhorn/Session.java
+++ src/main/java/de/inhorn/cybhorn/Session.java
@@ -14,15 +14,20 @@ public class Session {
 		switch (service) {
             case DATA:
 
+                // calculate throughput
                 int r = (int)Math.random()*pSubscriber.getTerminal().getSupportedRanTechnology().getRandom().length;
                 double random = pSubscriber.getTerminal().getSupportedRanTechnology().getRandom()[r];
+
+                // calculate dataUsed
                 double dataUsed = (double)pSubscriber.getTerminal().getSupportedRanTechnology().getMbit() * random * duration;
 
-                pSubscriber.setDataUsed(pSubscriber.getDaraUsed() - dataUsed);
+                if(dataUsed +  pSubscriber.getDataUsed() > pSubscriber.getSubscription().getDataVolume()) throw new IllegalArgumentsException("not enough data volume left");
+                else pSubscriber.setDataUsed(pSubscriber.getDataUsed() + dataUsed);
 				break;
 
-			case CALL:
-				pSubscriber.setSecondsCalled(pSubscriber.getSecondsCalled - duration);
+            case CALL:
+
+				pSubscriber.setSecondsCalled(pSubscriber.getSecondsCalled + duration);
 				break;
 
 			default:
