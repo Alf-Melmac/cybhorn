65e3b187c159a7e90c1506e5663575ff5efca70b
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-03-18 10:44:49 +0100

    Added subscriber name

--- src/main/java/de/inhorn/cybhorn/assembler/SubscriberAssembler.java
+++ src/main/java/de/inhorn/cybhorn/assembler/SubscriberAssembler.java
@@ -4,6 +4,7 @@ import de.inhorn.cybhorn.model.Subscriber;
 import de.inhorn.cybhorn.model.dtos.SubscriberPostDto;
 import de.inhorn.cybhorn.model.dtos.SubscriberViewDto;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,6 +24,8 @@ public class SubscriberAssembler {
 				.mcc(Integer.parseInt(subscriberPostDto.getMcc()))
 				.mnc(Integer.parseInt(subscriberPostDto.getMnc()))
 				.msin(Integer.parseInt(subscriberPostDto.getMsin()))
+				.firstName(subscriberPostDto.getFirstName())
+				.lastName(subscriberPostDto.getLastName())
 				.terminal(TerminalAssembler.fromDto(subscriberPostDto.getTerminal()))
 				.subscription(SubscriptionAssembler.fromDto(subscriberPostDto.getSubscription()))
 				.secondsCalled(subscriberPostDto.getSecondsCalled())
@@ -31,8 +34,22 @@ public class SubscriberAssembler {
 	}
 
 	public static SubscriberViewDto toDto(Subscriber subscriber) {
+		final String firstName = subscriber.getFirstName();
+		final String lastName = subscriber.getLastName();
+		String name = "";
+		if (StringUtils.isEmptyOrWhitespace(firstName)) {
+			if (!StringUtils.isEmptyOrWhitespace(lastName)) {
+				name = lastName;
+			}
+		} else if (StringUtils.isEmptyOrWhitespace(lastName)) {
+			name = firstName;
+		} else {
+			name = firstName + " " + lastName;
+		}
+
 		return SubscriberViewDto.builder()
 				.imsi(Long.toString(subscriber.getImsi()))
+				.name(name)
 				.terminal(TerminalAssembler.toDto(subscriber.getTerminal()))
 				.subscription(SubscriptionAssembler.toDto(subscriber.getSubscription()))
 				.secondsCalled(subscriber.getSecondsCalled())
