0d9046f41f974ee4772618afdc676a4a942eac35
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2021-03-18 15:08:29 +0100

    Initial test setup

---
+++ src/test/java/de/inhorn/cybhorn/model/SubscriberShould.java
@@ -0,0 +1,15 @@
+package de.inhorn.cybhorn.model;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Alf
+ * @since 18.03.2021
+ */
+class SubscriberShould {
+
+	@Test
+	void useData() {
+
+	}
+}
